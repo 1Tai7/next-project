2ade82bde5b6ae707b01cf59efd1b2348b9e9040
Ilce <ilsemilena1706@gmail.com>
2025-10-07 17:54:53 -0500

    add tailwind

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,6 +8,9 @@ export default function Home() {
       <p className="text-gray-700 text-lg">
         Has iniciado sesión correctamente.
       </p>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded shadow-lg transition duration-300 ease-in-out">
+        Haz Clic
+      </button>
     </div>
   );
 }
